9ec2dcee9058003471f20bf58755cb57a382a8e4
hyunil.jang <hyunil.jang@gangnamheroes.com>
2019-05-05 17:52:12 +0900

    add comment

--- src/queue/StackWithTwoQueues.java
+++ src/queue/StackWithTwoQueues.java
@@ -7,6 +7,9 @@ public class StackWithTwoQueues {
         queue1 = new LLQueue();
         queue2 = new LLQueue();
     }
+
+    //하나의 큐는 항상 비어있어야한다.
+    //push 연산은 항상 비어있지 않은 queue에 push한다.
     public void push(int data) {
         if (queue1.isEmpty()) {
             queue2.enQueue(data);
@@ -14,6 +17,9 @@ public class StackWithTwoQueues {
             queue1.enQueue(data);
         }
     }
+
+    // pop 연산 을 할 때 비어있지 않은 queue가 비어있는 queue에 하나씩 enqueue를 한다.
+    // enqueue한 quque의 가장 마지막 원소를 return 하면 stack의 pop과 같은 효과를 낼 수 있다.
 
     public int pop() {
         int i, size;
