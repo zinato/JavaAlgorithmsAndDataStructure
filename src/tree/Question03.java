0970a3600ce3096b922f12521c8d514ac283bd32
hyunil.jang <hyunil.jang@gangnamheroes.com>
2019-05-11 23:41:23 +0900

    Update Question03.java

--- src/tree/Question03.java
+++ src/tree/Question03.java
@@ -2,6 +2,13 @@ package tree;
 
 public class Question03 {
     //이진 트리의 항목을 검색하는 알고리즘을 구하라.
+    /*
+        이진트리에서 구하는 데이터가 있는 노드가 발견되면 참을 리턴함.
+        재귀적으로 트리 아래로 내려가면서 각 노드의 데이터와 비교해서 왼쪽이나 오른쪽을 선택한다.
+
+        시간 복잡도 : O(n)
+        공간 복잡도 : O(n)
+     */
     public boolean FindInBinaryTreeUsingRecursion(BinaryTreeNode root, int data) {
         boolean temp;
         //트리가 비었을 경우 데이터가 없으므로 false를 리턴한다.
